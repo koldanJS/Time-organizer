d24f7c863badf7daef9f2fdc7142011aa45549b2
koldanJS <koldan.js@gmail.com>
2022-04-20 15:17:57 +0300

    added redux and deleted all ;

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
 const container = document.querySelector('#root')
-const root = ReactDOM.createRoot(container);
+const root = ReactDOM.createRoot(container)
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
-reportWebVitals();
+reportWebVitals()
