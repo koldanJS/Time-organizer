23dd9714fe8c57c0671a63e54de96cb04f424a93
koldanJS <koldan.js@gmail.com>
2022-04-21 09:26:02 +0300

    Images have been transferred to the project, a component for exporting images has been created

---
+++ src/components/img/img.js
@@ -0,0 +1,27 @@
+import addLogo from '../../img/svg/add-logo.svg'
+import arrowLeft from '../../img/svg/arrow-left.svg'
+import arrowRight from '../../img/svg/arrow-right.svg'
+import arrowLogo from '../../img/svg/arrow-logo.svg'
+import calendarLogo from '../../img/svg/calendar-logo.svg'
+import clockLogo from '../../img/svg/clock-logo.svg'
+import clock from '../../img/svg/clock.svg'
+import homeLogo from '../../img/svg/home-logo.svg'
+import strike from '../../img/svg/strike.svg'
+import timerLogoWhite from '../../img/svg/timer-logo-white.svg'
+import timerLogo from '../../img/svg/timer-logo.svg'
+
+const images = {
+    addLogo,
+    arrowLeft,
+    arrowRight,
+    arrowLogo,
+    calendarLogo,
+    clockLogo,
+    clock,
+    homeLogo,
+    strike,
+    timerLogoWhite,
+    timerLogo
+}
+
+export default images
